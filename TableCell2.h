0d2ef0133cf0ae4348bca14fc2bb1d4fb412980d
Ronnieqwe <bmplayer1@gmail.com>
2014-02-02 02:04:04 +0200

    Create TableCell2.h

---
+++ TableCell2.h
@@ -0,0 +1,15 @@
+//
+//  TableCell2.h
+//  GetTweets
+//
+//  Created by admin on 01.02.14.
+//  Copyright (c) 2014 Eugene Bereza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableCell2 : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *message;
+
+@end

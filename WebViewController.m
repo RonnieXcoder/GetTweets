4a23f5641208c7d73f134e7cca08a6f942898b2e
Ronnieqwe <bmplayer1@gmail.com>
2014-02-02 13:10:53 +0200

    Create WebViewController.m

---
+++ WebViewController.m
@@ -0,0 +1,42 @@
+//
+//  WebViewController.m
+//  GetTweets
+//
+//  Created by admin on 01.02.14.
+//  Copyright (c) 2014 Eugene Bereza. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backToFeed:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end

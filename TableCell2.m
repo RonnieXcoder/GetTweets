c3760d91842408f6e6cc4f9fe72ec41597c5c6f1
Ronnieqwe <bmplayer1@gmail.com>
2014-02-02 02:05:00 +0200

    Create TableCell2.m

---
+++ TableCell2.m
@@ -0,0 +1,29 @@
+//
+//  TableCell2.m
+//  GetTweets
+//
+//  Created by admin on 01.02.14.
+//  Copyright (c) 2014 Eugene Bereza. All rights reserved.
+//
+
+#import "TableCell2.h"
+
+@implementation TableCell2
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
